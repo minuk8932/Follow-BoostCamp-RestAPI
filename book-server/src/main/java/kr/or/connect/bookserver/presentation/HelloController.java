821afa8fde192f491042633896b081f680d63597
minuk8932 <minuk8932@naver.com>
2018-04-19 17:44:12 +0900

    add BookService

--- book-server/src/main/java/kr/or/connect/bookserver/presentation/HelloController.java
+++ book-server/src/main/java/kr/or/connect/bookserver/presentation/HelloController.java
@@ -1,4 +1,4 @@
-package kr.or.connect.presentation;
+package kr.or.connect.bookserver.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ book-server/src/main/java/kr/or/connect/bookserver/service/BookService.java
@@ -0,0 +1,31 @@
+package kr.or.connect.bookserver.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.connect.domain.Book;
+
+/**
+ * 
+ * @author minchoba
+ * BookService 클래스
+ * 요청의 흐름: @Controller가 붙은 클래스 -> @Service가 붙은 클래스 -> @Repository가 붙은 클래스
+ * 
+ */
+
+@Service
+public class BookService {
+	
+	public Book findById(Integer id) {
+		return new Book(1, "자바 이렇게 공부하자", "김자바", 300);
+	}
+	
+	public Collection<Book> findAll(){
+		return Arrays.asList(
+			new Book(1, "네이버 네비 좋아요", "김광근", 300),
+			new Book(2, "HTTP 완벽 이해하기", "김명호", 300)
+		);
+	}
+}
