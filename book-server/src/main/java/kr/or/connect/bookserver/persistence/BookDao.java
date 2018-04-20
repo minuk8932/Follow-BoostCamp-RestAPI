1e132aaab5b56a33756dbc7e01e19a354c3f0325
minuk8932 <minuk8932@naver.com>
2018-04-20 16:35:12 +0900

    merge BookDao from P project spring-practice

---
+++ book-server/src/main/java/kr/or/connect/bookserver/persistence/BookDao.java
@@ -0,0 +1,95 @@
+package kr.or.connect.bookserver.persistence;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import kr.or.connect.domain.Book;
+
+/**
+ * 
+ * 	@author minchoba
+ *	DataSource에 의존하는 DAO(Data Access Object) 추출
+ *	DB에 접속해 쿼리를 호출하는 역할을 해당 클래스에 선언해줌 
+ *	즉, 행의 수를 세는 기능을 본 클래스로 옮기고 메인 클래스에선 결과값만 받아 볼 수 있게 설정
+ */
+
+@Repository
+public class BookDao {
+	private static final String COUNT_BOOK = "SELECT COUNT(*) FROM book";
+	private static final String SELECT_BY_ID = "SELECT id, title, author, pages FROM book where id = :id"; 	// book 테이블을 id로 조회
+	private static final String DELETE_BY_ID = "DELETE FROM book WHERE id= :id";		// 삭제 쿼리
+	private static final String UPDATE =
+			"UPDATE book SET\n"
+			+ "title = :title,"
+			+ "author = :author,"
+			+ "pages = :pages\n"
+			+ "WHERE id = :id";
+	
+	private NamedParameterJdbcTemplate jdbc;
+	private SimpleJdbcInsert insertAction;
+	
+	public BookDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);	// NamedParameterJdbcTemplate에 파라미터로 받아온 dataSource를 넘겨줌
+		
+		this.insertAction = new SimpleJdbcInsert(dataSource)		// jdbcInsert 객체 생성 후 메서드를 통해 dataSource를 가져옴
+				.withTableName("book")							// table 이름이 book
+				.usingGeneratedKeyColumns("id");					// 프라이머리키가 id
+	}
+	
+	public int countBooks() {								// 책 갯수를 세는 메소드
+		Map<String, Object> params = Collections.emptyMap();	// BookLauncher 클래스의 코드를 그대로 가져옴, SQL 구문은 상수 처리
+		return jdbc.queryForObject(COUNT_BOOK, params, Integer.class);	// 메소드 실행을 통해, 받아온 행 데이터를 객체로 받아와 그 수를 정수로 반환
+	}
+	
+	/**
+	 * 
+	 * 		RowMapper<Book> rowMapper = new RowMapper<Book>() {
+	 *			@Override
+	 *			public Book mapRow(ResultSet rs, int i) throws SQLException {
+	 *				Book book = new Book();
+	 *				book.setId(rs.getInt("id"));
+	 *				book.setTitle(rs.getString("title"));
+	 *				book.setAuthor(rs.getString("author"));
+	 *				book.setPages((Integer) rs.getObject("pages"));
+	 *				return book;
+	 *			}
+	 *		};
+	 * 				->  아래의 람다 표현식을 익명클래스로 표현
+	 */
+	
+	RowMapper<Book> rowMapper = BeanPropertyRowMapper.newInstance(Book.class);	// 람다 표현식을 간단히해주는 BeanPropertyRowMapper로 선언
+																// newInstance로 생성된 객체는 멀티스레드에서 접근해도 안전해, 멤버변수로 선언 가능
+	public Book selectById(Integer id) {		
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", id);
+		
+		return jdbc.queryForObject(SELECT_BY_ID, params, rowMapper);	// id값으로 조회하는 쿼리, rowMapper를 통해 객체 형변환 
+	}
+	
+	public int deleteById(Integer id) {
+		Map<String, ?> params = Collections.singletonMap("id", id);
+		return jdbc.update(DELETE_BY_ID, params);
+	}
+	
+	public Integer insert(Book book) {
+		// BeanPropertySqlParameterSource을 이용해서 Book 클래스에서 SqlParameterSource로 변환
+		SqlParameterSource params = new BeanPropertySqlParameterSource(book);	
+		return insertAction.executeAndReturnKey(params).intValue();			// 생성된 key 값을 정수형으로 반환
+	}
+	
+	public int update(Book book) {											// update
+		SqlParameterSource params = new BeanPropertySqlParameterSource(book);
+		return jdbc.update(UPDATE, params);
+	}
+}
---
+++ book-server/src/test/java/kr/or/connect/bookserver/persistence/BookDaoTest.java
@@ -0,0 +1,84 @@
+package kr.or.connect.bookserver.persistence;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.connect.BookServerApplication;
+import kr.or.connect.domain.Book;
+
+/**
+ * 
+ * @author minchoba
+ * Appconfig에 있던 요소들이 Spring Boot에 의해 자동으로 처리
+ * 자동 처리 항목-------------------------------------------------------
+ * @ComponentScan 선언
+ * @PropertSource 선언으로 application.properties에 있던 속성을 값을 읽어오기
+ * dataSource에 application.properties에 있던 속성을 주입하고 @Bean
+ * transactionManager를 @Bean 으로 등록
+ * ------------------------------------------------------------------
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BookServerApplication.class)
+@Transactional
+public class BookDaoTest {
+	@Autowired
+	private BookDao dao;
+	
+	@Test
+	public void shouldCount() {
+		int count = dao.countBooks();
+		System.out.println(count);
+	}
+	
+	@Test
+	public void shouldInsertAndSelect() {
+		// given
+		Book book = new Book("Java 웹개발", "네이버", 342);
+
+		// when
+		Integer id = dao.insert(book);
+
+		// then
+		Book selected = dao.selectById(id);
+		System.out.println(selected);
+		assertThat(selected.getTitle(), CoreMatchers.is("Java 웹개발"));
+	}
+	
+	@Test
+	public void shouldDelete() {
+		// given
+		Book book = new Book("네이버 자바", "네이버", 142);
+		Integer id = dao.insert(book);
+
+		// when
+		int affected = dao.deleteById(id);
+
+		// Then
+		assertThat(affected, CoreMatchers.is(1));
+	}
+	
+	@Test
+	public void shouldUpdate() {
+		// Given
+		Book book = new Book("네이버 자바", "네이버", 142);
+		Integer id = dao.insert(book);
+
+		// When
+		book.setId(id);
+		book.setTitle("네이버 자바2");
+		int affected = dao.update(book);
+
+		// Then
+		assertThat(affected, CoreMatchers.is(1));
+		Book updated = dao.selectById(id);
+		assertThat(updated.getTitle(), CoreMatchers.is("네이버 자바2"));
+	}
+}
